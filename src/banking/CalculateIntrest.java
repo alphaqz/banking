9c434b9e6afe6a0f5f9c6969e42cbadae7068de5
alphaqz <kaungthuhankth@gmail.com>
2023-03-30 06:17:34 +0630

    Update CalculateIntrest.java

--- src/banking/CalculateIntrest.java
+++ src/banking/CalculateIntrest.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.JOptionPane;
 
@@ -21,7 +22,7 @@ public class CalculateIntrest {
 	//private static int total = 0;  
 	//private static String id = "";
 	public static void main(String[] args) {
-	 int output =	something("ac-0000003");
+	 int output =	something("ac-0000004");
 	 System.out.println(output);
 	}
 	public static int something(String accid) {
@@ -29,11 +30,11 @@ public class CalculateIntrest {
 		System.out.println("calculating intrest");		
 		String str[] ;
 		  str = new String[3];
-		Date currDate = null;
+		Date prevDate = null;
 		int currMonth = 0;
 		int todayTotal = 0;
 		double intrestTotal = 0.0;
-		
+		double prevAmount = 0.0;
 		 try
          {
         	 con=connect.getConnection();
@@ -51,7 +52,56 @@ public class CalculateIntrest {
              
              while(rs.next())
              {
-            	 //System.out.println("new loop");
+            	 Date currDate = rs.getDate(3);
+            	 String id = rs.getString(1);
+            	 LocalDate localDate = currDate.toLocalDate();
+            	 int tempMonth = localDate.getMonthValue();
+            	 int day = localDate.getDayOfMonth();
+            	 double currAmount = Integer.parseInt(rs.getString(2));
+            	 double currIntrest = getIntrest(currAmount);
+            	 
+            	 if(currDate !=null && prevDate != null) {            		 
+	            	 long diffInMillies = Math.abs(currDate.getTime() - prevDate.getTime());
+	            	 long diff = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
+	            	 System.out.println(diff);
+	            	 
+	            	 intrestTotal+= (diff * getIntrest(prevAmount));
+            	 }
+            	    
+            	 if(id.startsWith("D")) {
+            		 //deposit
+            		 todayTotal += currAmount;
+            	 }else {
+            		 //withdraw
+            		 todayTotal -= currAmount;
+            	 }
+            	 
+            	 intrestTotal+= currIntrest;
+            	 prevDate = currDate;
+            	 prevAmount = currAmount;            	 
+            	 System.out.println(prevDate + " intrest is " + getIntrest(total));
+             }
+             System.out.println("total amount is : "+total);
+             System.out.println("total intrest is : "+intrestTotal);
+             
+         }catch(SQLException e)
+         {
+             JOptionPane.showMessageDialog(null,e.getMessage());
+         }
+		 return total;
+	}
+//	public static int getTotal(String idinput) {
+//		id = idinput;
+//		something();
+//		return total;
+//	}
+	public static double getIntrest(double currAmount) {
+		return Math.floor( currAmount * 0.0001);
+	}
+	
+	{
+		/*
+	  //System.out.println("new loop");
             	// System.out.println("wtf");
             	 String id = rs.getString(1);
             	 Date tempDate = rs.getDate(3);
@@ -104,25 +154,8 @@ public class CalculateIntrest {
                      str[i]=rs.getString(i+1);                     
                      //System.out.print( rs.getString(i+1) + " ");                    
                  }
-             }
-             intrestTotal+=getIntrest(todayTotal);
-             System.out.println(currDate + " intrest is " + getIntrest(todayTotal));
-             System.out.println("total amount is : "+total);
-             System.out.println("total intrest is : "+intrestTotal);
-             
-         }catch(SQLException e)
-         {
-             JOptionPane.showMessageDialog(null,e.getMessage());
-         }
-		 return total;
-	}
-//	public static int getTotal(String idinput) {
-//		id = idinput;
-//		something();
-//		return total;
-//	}
-	public static double getIntrest(int amount) {
-		return Math.floor( amount * 0.0001);
+	  */
+	
 	}
 
 }
